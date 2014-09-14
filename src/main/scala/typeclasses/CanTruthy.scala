84c7827c3901c97ea9d907513ae33ea3f176b32b
Shokunin San <me@flatmap.io>
2014-09-14 13:20:27 +0200

    Day 1: Added some tests and truthyIf control structure.

--- src/main/scala/typeclasses/CanTruthy.scala
+++ src/main/scala/typeclasses/CanTruthy.scala
@@ -6,38 +6,63 @@ package typeclasses
 
 // Type class
 trait CanTruthy[A] { self =>
+
   def truthys(a: A): Boolean
+
 }
 
 object CanTruthy {
-  def truthys[A](f: A => Boolean): CanTruthy[A] = new CanTruthy[A] {
-    def truthys(a: A): Boolean = f(a)
-  }
+
+  def truthys[A](f: A => Boolean): CanTruthy[A] =
+    new CanTruthy[A] {
+     def truthys(a: A): Boolean = f(a)
+    }
+
 }
 
 trait CanTruthyOps[A] {
+
   def self: A                  // to be defined in instance
+
   implicit def F: CanTruthy[A] // to be defined in instance
+
   def truthy: Boolean = F.truthys(self)
+
 }
 
 object ToCanIsTruthyOps {
+
   implicit def toCanIsTruthyOps[A](v: A)(implicit ev: CanTruthy[A]) =
     new CanTruthyOps[A] {
       def self = v
       implicit def F: CanTruthy[A] = ev
     }
+
 }
 
+// Type class instances
 object CanTruthyInstances {
+
   implicit val intCanTruthy: CanTruthy[Int] = CanTruthy.truthys({
     case 0 => false
     case _ => true
   })
+
   implicit def listCanTruthy[A]: CanTruthy[List[A]] = CanTruthy.truthys({
     case Nil => false
     case _   => true
   })
+
   implicit val nilCanTruthy: CanTruthy[scala.collection.immutable.Nil.type] = CanTruthy.truthys(_ => false)
+
   implicit val booleanCanTruthy: CanTruthy[Boolean] = CanTruthy.truthys(identity)
+
+}
+
+// Custom control structures
+object CanTruthyControls {
+
+  def truthyIf[A:CanTruthy, B, C](a: A)(trueCase: => B)(falseCase: => C) =
+    if (implicitly[CanTruthy[A]].truthys(a)) trueCase else falseCase
+
 }
--- src/test/scala/typeclasses/CanTruthySpec.scala
+++ src/test/scala/typeclasses/CanTruthySpec.scala
@@ -6,19 +6,43 @@ import CanTruthyInstances._
 
 class CanTruthySpec extends FlatSpec with Matchers {
 
-  "Int.truthy" should "return an approporiate boolean value" in {
+  "1.truthy" should "return true" in {
     1.truthy should be === true
   }
 
-  "List.truthy" should "return an appropriate boolean value" in {
+  "0.truthy" should "return false" in {
+    0.truthy should be === false
+  }
+
+  "List.truthy" should "return true" in {
     List("foo").truthy should be === true
   }
 
-  "Nil.truthy" should "return an appropriate boolean value" in {
+  "Nil.truthy" should "return false" in {
     Nil.truthy should be === false
   }
 
-  "Boolean.truthy" should "return an appropriate boolean value" in {
+  "false.truthy" should "return false" in {
     true.truthy should be === true
+  }
+
+  "true.truthy" should "return true" in {
+    true.truthy should be === true
+  }
+
+  "truthyIf" should "be able to evaluate CanTruthy instances" in {
+    import CanTruthyControls._
+
+    // For integers
+    (truthyIf(1) { "yes" } { "no" }) should be === "yes"
+    (truthyIf(0) { "yes" } { "no" }) should be === "no"
+
+    // For Lists
+    (truthyIf(List(1,2,3)) { "yes" } { "no" }) should be === "yes"
+    (truthyIf(Nil) { "yes" } { "no" }) should be === "no"
+
+    // For booleans
+    (truthyIf(true) { "yes" } { "no" }) should be === "yes"
+    (truthyIf(false) { "yes" } { "no" }) should be === "no"
   }
 }
